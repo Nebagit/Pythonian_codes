f179d1e1ed9534cfad0058689c2490af08f42415
Nebagit <neba.t.git@gmail.com>
2025-06-20 14:57:53 +0300

    an exerise on dictionaries of python

---
+++ dictionaries.py
@@ -0,0 +1,5 @@
+# dictionaries in Python are mutable, unrdered collections  of key-value pairs
+# they are defined using curly braces {} or the dict() constructor
+
+nebiyu = {"name": "Nebiyu", "school": "Addis Ababa University", "age": 22, "experience": 2}
+print(nebiyu)

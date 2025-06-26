7bdfd2f6b37a6998be315ce4208d987baecf177e
Nebagit <neba.t.git@gmail.com>
2025-06-26 12:35:26 +0300

    End of Dictionary

--- dictionaryexercise.py
+++ dictionaryexercise.py
@@ -21,3 +21,32 @@ for pet in pets:
     print(f"Name: {pet['name']}")
     print(f"Age: {pet['age']}")
     print()  # Print a newline for better readability
+
+# Favorite places dictionary
+favorite_places = {
+    'Dawit': ['Addis Ababa', 'Bahir Dar', 'Wollo'],
+    'Melaku': ['Addis Ababa', 'Debre Tabor'],
+    'Yekoye': ['Debre Tabor', 'Lalibela']
+}
+
+for favoriteofperson in favorite_places:
+    print(f"{favorite_places[favoriteofperson]} are the Favorite Place {favoriteofperson} want to see")
+
+
+favorite_numbers = {
+'Dawit': [1,2,3],
+'Melaku': [3,4,5,6],
+'Yekoye': [12,23,34,45]
+}
+
+for favnum in favorite_numbers:
+    print(f"{favorite_numbers[favnum]} are the Numbers {favnum} Likes")
+
+
+cities={
+'Addis Ababa': {'Country': 'Ethiopia', 'Admin': 'Adancch', 'Population': '10000000', 'Fact': 'Very busy, and much diversities'},
+'Bahrar': {'Country': 'Ethiopia', 'Admin': 'Melaku', 'Population': '1234567890', 'Fact': 'Very smart, and interesting peoples'}
+}
+
+for city in cities:
+    print(f"City Name: {city}\n The details of the city: {cities[city]}")

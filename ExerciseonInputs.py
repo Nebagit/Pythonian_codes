1f361aa3ff1022ef7b8cc6dd5ea72361602d750a
Nebagit <neba.t.git@gmail.com>
2025-06-30 10:56:21 +0300

    inputs finished

--- ExerciseonInputs.py
+++ ExerciseonInputs.py
@@ -6,3 +6,9 @@ if int(Restaurantseating) > 8:
     print("You will have to wait for a table.")
 else:
     print("Your table is ready.")
+
+number = input("Enter a number: ")
+if int(number) % 10 == 0:
+    print(f"{number} is a multiple of 10.")
+else:
+    print(f"{number} is not a multiple of 10.")

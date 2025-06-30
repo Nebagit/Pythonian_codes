a397a613aba90fbe8c7f71c5ebd2f4d44779f954
Nebagit <neba.t.git@gmail.com>
2025-06-30 10:54:18 +0300

    inputs restaurant

--- ExerciseonInputs.py
+++ ExerciseonInputs.py
@@ -1,2 +1,8 @@
 car = input("What kind of rental car would you like? ")
 print(f"Let me see if I can find you a {car}.")
+
+Restaurantseating = input("How many people are in your dinner group? ")
+if int(Restaurantseating) > 8:
+    print("You will have to wait for a table.")
+else:
+    print("Your table is ready.")

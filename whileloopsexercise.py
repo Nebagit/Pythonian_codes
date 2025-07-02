3383af9d8f8cf6fcf3d5e000456954320712aa24
Nebagit <neba.t.git@gmail.com>
2025-07-02 16:56:15 +0300

    while exercise

--- whileloopsexercise.py
+++ whileloopsexercise.py
@@ -8,3 +8,11 @@ while unconfirmed_users:
 print("\nThe following users have been confirmed:")
 for confirmed_user in confirmed_users:
     print(confirmed_user.title())
+
+pets = ['dog', 'cat', 'goldfish', 'rabbit', 'hamster', 'cat', 'cat', 'cat', 'cat']
+while 'cat' in pets:
+    pets.remove('cat')
+
+print("\nUpdated list of pets:")
+for pet in pets:
+    print(pet.title())
